816fb871d9f7d411bfe1046d2b56f0a78ebd19b8
franckpalide <franck.palide@gmail.com>
2025-10-24 07:04:02 +0200

    setting pour deployage pythonAnywhere

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-l^dpi1qnb&nedozxit+e3&667xf(x6g31pb#=+1gom)#%o*((6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
